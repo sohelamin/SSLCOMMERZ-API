9dcfc52992de5ab156b8c284e95e67eac063691a
sohelamin <sohelamincse@gmail.com>
2014-07-17 16:34:04 +0600

    Api Added
    SSLCOOMERZ Payment Gateway API

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>SSL Commerce Example</title>
+</head>
+<body>
+	<form action="https://www.sslcommerz.com.bd/testbox/process/index.php" method="post" name="form1">
+		<input type="hidden" name="store_id" value="test" /> <!— You have to put your store ID here - >
+		<input type="hidden" name="total_amount" value="800" />
+		<input type="hidden" name="tran_id" value="ftrans01" />
+		<?php $itemid='fbfan01'; // Set your item/product id here ?>
+		<input type="hidden" name="success_url" value="http://facebookcorner.com/api_test/notify.php?itemid=<?php echo $itemid;?>" />
+		<input type="hidden" name="fail_url" value="http://facebookcorner.com/api_test/fail.php?itemid=<?php echo $itemid;?>" />
+		<input type="hidden" name="cancel_url" value="http://facebookcorner.com/api_test/cancel.php?itemid=<?php echo $itemid;?>" />
+		<input type="submit" value="Pay with SSLCOMMERZ" name="pay">
+	</form>	
+</body>
+</html>
